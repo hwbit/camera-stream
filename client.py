c80ca23bb3df0b4fe73f9e5137100d5c8f924e5b
hwbit <83245042+hwbit@users.noreply.github.com>
2024-03-18 16:22:44 -0500

    init

---
+++ client.py
@@ -0,0 +1,55 @@
+import cv2
+import socket
+import pickle
+import struct
+
+# Create a socket client
+class Client():
+    def __init__(self):
+        self.video_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.video_client_socket.connect(("10.0.0.98", 9999))  # Replace with the server’s IP address
+        # self.video_client_socket.connect(("10.0.0.102", 9999))  # Replace with the server’s IP address
+
+        self.received_data = b""
+        self.payload_size = struct.calcsize("<L")
+
+    def run(self):
+        print("Loading Video")
+
+        while True:
+            # Receive and assemble the data until the payload size is reached
+            while len(self.received_data) < self.payload_size:
+                self.received_data += self.video_client_socket.recv(4096)
+
+            # Extract the packed message size
+            packed_msg_size = self.received_data[:self.payload_size]
+            self.received_data = self.received_data[self.payload_size:]
+            msg_size = struct.unpack("<L", packed_msg_size)[0]
+
+            # Receive and assemble the frame data until the complete frame is received
+            while len(self.received_data) < msg_size:
+                self.received_data += self.video_client_socket.recv(4096)
+
+            # Extract the frame data
+            frame_data = self.received_data[:msg_size]
+            self.received_data = self.received_data[msg_size:]
+
+            # Deserialize the received frame
+            received_frame = pickle.loads(frame_data)
+
+            # Display the received frame
+            cv2.imshow('Client Video', received_frame)
+
+            # Press ‘q’ to quit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Release resources
+        cv2.destroyAllWindows()
+        self.video_client_socket.close()
+  
+# end client
+      
+if __name__ == "__main__":
+    c = Client()
+    c.run()
---
+++ server.py
@@ -0,0 +1,38 @@
+import cv2
+import socket
+import pickle
+import struct
+
+
+
+class Server():
+    def __init__(self):
+        print("Starting...")
+        self.video_capture = cv2.VideoCapture(0)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('', 9999))  
+        self.server_socket.listen(10)
+
+        self.client_socket, self.client_address = self.server_socket.accept()
+        print(f"[*] Accepted connection from {self.client_address}")
+        
+    def run(self):
+        while True:
+            ret, frame = self.video_capture.read()
+            serialized_frame = pickle.dumps(frame)
+            message_size = struct.pack("L", len(serialized_frame))
+            self.client_socket.sendall(message_size + serialized_frame)
+            cv2.imshow('Server Video', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        self.video_capture.release()
+        self.cv2.destroyAllWindows()
+            
+#end class
+
+
+if __name__ == "__main__":
+    s = Server()
+    while True:
+        s.run()
